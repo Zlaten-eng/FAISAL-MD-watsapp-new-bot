4371d4bfc35c3d2723bbaec4e5161edc69ba179a
Tohid Khan <tohidkhan9050482152@gmail.com>
2025-06-03 22:04:10 +0530

    Mr Tohid

--- config.js
+++ config.js
@@ -23,7 +23,7 @@ ANTI_LINK: process.env.ANTI_LINK || "true",
 // make anti link true,false for groups 
 MENTION_REPLY: process.env.MENTION_REPLY || "false",
 // make true if want auto voice reply if someone menetion you 
-MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/wtqlhp.jpg",
+MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://qu.ax/bfZVV.jpg",
 // add custom menu and mention reply image url
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
@@ -43,7 +43,7 @@ OWNER_NAME: process.env.OWNER_NAME || "SHAH FAISAL",
 // add bot owner name
 DESCRIPTION: process.env.DESCRIPTION || "> © 𝚸𝚯𝐖𝚵𝚪𝐃 𝚩𝐘 𝐅𝚫𝚰𝐒𝚫𝐋-𝚳𝐃",
 // add bot owner name    
-ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/wtqlhp.jpg",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://qu.ax/bfZVV.jpg",
 // add img for alive msg
 LIVE_MSG: process.env.LIVE_MSG || "© 𝚸𝚯𝐖𝚵𝚪𝐃 𝚩𝐘 𝐅𝚫𝚰𝐒𝚫𝐋-𝚳𝐃",
 // add alive msg here 
