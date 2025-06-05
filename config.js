09710007c21a586a896b379337490a75f7b908d7
Zlaten-eng <ronojepsat70@gmail.com>
2025-06-05 10:57:22 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "IK~7clWwBTT#1NJPcqoaWgu-LXz3w6-WzcC0SlVfGonkIySlgEtKlAU",
+SESSION_ID: process.env.SESSION_ID || "ROVER-XMD~42VWgJ6B#UqfsMiEIYXZtHPbiMdpJG4RRqUzply1W7JVriNzKfc8",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -25,7 +25,7 @@ MENTION_REPLY: process.env.MENTION_REPLY || "false",
 // make true if want auto voice reply if someone menetion you 
 MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://qu.ax/bfZVV.jpg",
 // add custom menu and mention reply image url
-PREFIX: process.env.PREFIX || ".",
+PREFIX: process.env.PREFIX || "?",
 // add your prifix for bot   
 BOT_NAME: process.env.BOT_NAME || "𝐅𝚫𝚰𝐒𝚫𝐋-𝚳𝐃",
 // add bot namw here for menu
@@ -37,7 +37,7 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "923008340829",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "254788409105",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "SHAH FAISAL",
 // add bot owner name
@@ -71,7 +71,7 @@ AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "94752677804",
+DEV: process.env.DEV || "254788409105",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
